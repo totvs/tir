c196d6f6a9905d32ea743418352b86ed30ee5711
Heitor Marsolla <hmarsolla@hotmail.com>
2018-08-31 17:52:58 -0300

    enum docs

--- tir/technologies/core/enumerations.py
+++ tir/technologies/core/enumerations.py
@@ -5,8 +5,11 @@ class MessageType(Enum):
     Enumeration that represents the standard messages that are emitted inside log file.
 
     CORRECT = 1
+
     INCORRECT = 2
+
     DISABLED = 3
+
     ASSERTERROR = 4
     '''
     CORRECT = 1
@@ -19,9 +22,13 @@ class ScrapType(Enum):
     Enumeration that represents the WebScraping type to be used.
 
     TEXT = 1
+
     CSS_SELECTOR = 2
+
     MIXED = 3
+
     SCRIPT = 4
+
     XPATH = 5
     '''
     TEXT = 1
@@ -35,7 +42,9 @@ class ClickType(Enum):
     Enumeration that represents the different types of Click logic.
 
     JS = 1
+
     SELENIUM = 2
+
     ACTIONCHAINS = 3
     '''
     JS = 1
