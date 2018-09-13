ac296b7ed9012077051b7f7dd2d837446e7c4ad6
Heitor Marsolla <hmarsolla@hotmail.com>
2018-09-13 14:15:20 -0300

    Changed version from Sphinx

--- doc_files/source/conf.py
+++ doc_files/source/conf.py
@@ -27,7 +27,7 @@ author = 'Totvs Automation Team'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
