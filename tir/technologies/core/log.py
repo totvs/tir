461229a0af61ae21bf08a0bd3ec0c0cef3955de5
Renan Lacerda Lisboa <renanllisboa@gmail.com>
2019-03-20 18:16:22 -0300

    change write csv method and fix log quotechar.

--- tir/technologies/core/log.py
+++ tir/technologies/core/log.py
@@ -3,6 +3,8 @@ import os
 import numpy as nump
 import pandas as panda
 import uuid
+import csv
+import inspect
 from datetime import datetime
 
 class Log:
@@ -14,7 +16,7 @@ class Log:
     >>> # Instanted inside base.py:
     >>> self.log = Log()
     """
-    def __init__(self, user="", station="", program="", program_date=time.strftime("01/01/1980 12:00"), version="", release="", database="", issue="", execution_id="", country="", folder="", test_type="TIR"):
+    def __init__(self, user="", station="", program="", program_date=time.strftime("01/01/1980 12:00:00"), version="", release="", database="", issue="", execution_id="", country="", folder="", test_type="TIR"):
         self.timestamp = time.strftime("%Y%m%d%H%M%S")
 
         self.user = user
@@ -62,11 +64,11 @@ class Log:
         >>> self.log.new_line(True, "Success")
         """
         line = []
-        total_cts = "1"
-        passed = "1" if result else "0"
-        failed = "0" if result else "1"
+        total_cts = 1
+        passed = 1 if result else 0
+        failed = 0 if result else 1
 
-        line.extend([time.strftime("%d/%m/%y %X"), self.user, self.station, self.program, self.program_date, total_cts, passed, failed, self.seconds, self.version, self.release, message, self.database, self.issue, self.execution_id, self.country, self.test_type])
+        line.extend([time.strftime("%d/%m/%Y %X"), self.user, self.station, self.program, self.program_date, total_cts, passed, failed, self.seconds, self.version, self.release, message, self.database, self.issue, self.execution_id, self.country, self.test_type])
         self.table_rows.append(line)
 
     def save_file(self, filename):
@@ -82,7 +84,6 @@ class Log:
         log_file = f"{self.user}_{uuid.uuid4().hex}_auto.csv"
                 
         if len(self.table_rows) > 0:
-            data = nump.array(self.table_rows)
             try:
                 if self.folder:
                     path = f"{self.folder}\\{self.station}\\{log_file}"
@@ -92,12 +93,16 @@ class Log:
                     os.makedirs(f"Log\\{self.station}")
             except OSError:
                 pass
-                
-            df = panda.DataFrame(data, columns=data[0])
-            df.drop(0, inplace=True)
-            df.to_csv(path, index=False, sep=';', encoding='latin-1')
+            
+            testcases = self.list_of_testcases()
 
-            print(f"Log file created successfully: {path}")
+            if len(self.table_rows[1:]) == len(testcases):
+                with open(path, mode="w", newline="") as csv_file:
+                    csv_writer = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+                    for line in self.table_rows:
+                        csv_writer.writerow(line)
+
+                print(f"Log file created successfully: {path}")
 
     def set_seconds(self):
         """
@@ -110,3 +115,10 @@ class Log:
         """
         delta = datetime.today() - self.initial_time
         self.seconds = round(delta.total_seconds(), 2)
+
+    def list_of_testcases(self):
+        """
+        Returns a list of test cases from suite 
+        """
+        runner = next(iter(list(filter(lambda x: "runner.py" in x.filename, inspect.stack()))))
+        return list(runner.frame.f_locals['test'])
