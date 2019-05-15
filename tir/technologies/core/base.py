5b7c38d9c4d9da4ac2160488a4f5fb43fdedeacf
Renan Lacerda Lisboa <renanllisboa@gmail.com>
2019-05-15 19:53:44 -0300

    insert home and clear key control in send_keys.

--- tir/technologies/core/base.py
+++ tir/technologies/core/base.py
@@ -635,12 +635,18 @@ class Base(unittest.TestCase):
         try:
             element.send_keys("")
             element.click()
+            if arg.isprintable():
+                element.clear()
+            element.send_keys(Keys.HOME)
             element.send_keys(arg)
         except Exception:
             actions = ActionChains(self.driver)
             actions.move_to_element(element)
             actions.send_keys("")
             actions.click()
+            if arg.isprintable():
+                actions.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.DELETE)
+            actions.send_keys(Keys.HOME)
             actions.send_keys(arg)
             actions.perform()
 
