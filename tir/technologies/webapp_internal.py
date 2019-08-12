8acb29ed02ecd87dce79eeedb028795a7a343326
Wilson08 <wilson.ortega08@gmail.com>
2019-08-12 17:15:06 -0300

    Fix ClickLabel

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -4063,16 +4063,20 @@ class WebappInternal(Base):
         >>> # Call the method:
         >>> oHelper.ClickLabel("Search")
         """
+        label = ''
         self.wait_element(label_name)
 
         container = self.get_current_container()
         if not container:
             self.log_error("Couldn't locate container.")
-
-        labels = container.select("label")
-        filtered_labels = list(filter(lambda x: label_name.lower() in x.text.lower(), labels))
-        filtered_labels = list(filter(lambda x: EC.element_to_be_clickable((By.XPATH, xpath_soup(x))), filtered_labels))
-        label = next(iter(filtered_labels), None)
+        
+        endtime = time.time() + self.config.time_out
+        while(not label and time.time() < endtime):
+            labels = container.select("label")
+            filtered_labels = list(filter(lambda x: label_name.lower() in x.text.lower(), labels))
+            filtered_labels = list(filter(lambda x: EC.element_to_be_clickable((By.XPATH, xpath_soup(x))), filtered_labels))
+            label = next(iter(filtered_labels), None)
+            
         if not label:
             self.log_error("Couldn't find any labels.")
 
