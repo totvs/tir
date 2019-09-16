9bc21c71c56285300c5464989535b118183304fc
Wilson08 <wilson.ortega08@gmail.com>
2019-09-16 14:33:03 -0300

    Fix user-screen method empty password

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -278,7 +278,7 @@ class WebappInternal(Base):
         password_value = self.get_web_value(password())
         endtime = time.time() + self.config.time_out
         try_counter = 0
-        while (time.time() < endtime and not password_value.strip()):
+        while (time.time() < endtime and not password_value.strip() and self.config.password != ''):
 
             if try_counter == 0:
                 password = lambda: self.soup_to_selenium(password_element)
@@ -295,7 +295,7 @@ class WebappInternal(Base):
             self.wait_blocker_ajax()
             try_counter += 1 if(try_counter < 1) else -1
         
-        if not password_value.strip():
+        if not password_value.strip() and self.config.password != '':
             self.restart_counter += 1
             self.log_error("Couldn't fill User input element.")
 
