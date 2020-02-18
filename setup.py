318edaefb86b5998f234fed45db502716955f187
Renan Lacerda Lisboa <renanllisboa@gmail.com>
2020-02-18 11:28:52 -0300

    update files

--- setup.py
+++ setup.py
@@ -6,11 +6,28 @@ except ImportError:
 
 config = {
     'description': 'TOTVS Interface Robot',
+    'long_description': 'TIR is a Python module used to create test scripts for web interfaces. With it, you are able to easily create and execute test suites and test cases for any supported Totvs\' web interface systems, such as Protheus Webapp.',
     'author': 'TOTVS Automation Team',
     'url': 'https://github.com/totvs/tir',
     'download_url': 'https://github.com/totvs/tir',
+    'project_urls':{
+    'Script Samples': 'https://github.com/totvs/tir-script-samples'
+    },
     'author_email': '',
-    'version': '1.13.0',
+    'version': '1.13.0', 
+    'license': 'MIT',
+    'keywords': 'test automation selenium tir totvs protheus framework',
+    'classifiers':[
+        'Environment :: Web Environment',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing'
+    ],
     'install_requires': [
         'beautifulsoup4==4.7.1',
         'bs4==0.0.1',
