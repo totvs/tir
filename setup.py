1b1d0fe92c0cedf02d72ea2072e3252eeefa9327
Renan Lacerda Lisboa <renanllisboa@gmail.com>
2020-02-20 11:18:19 -0300

    update file

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ try:
 except ImportError:
     from distutils.core import setup
 
-config = {
-    'description': 'TOTVS Interface Robot',
-    'long_description': 'TIR is a Python module used to create test scripts for web interfaces. With it, you are able to easily create and execute test suites and test cases for any supported Totvs\' web interface systems, such as Protheus Webapp.',
-    'author': 'TOTVS Automation Team',
-    'url': 'https://github.com/totvs/tir',
-    'download_url': 'https://github.com/totvs/tir',
-    'project_urls':{
+setup(
+    description='TOTVS Interface Robot',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='tir_engpro',
+    url='https://github.com/totvs/tir',
+    download_url='https://github.com/totvs/tir',
+    project_urls={
     'Script Samples': 'https://github.com/totvs/tir-script-samples'
     },
-    'author_email': '',
-    'version': '1.13.0', 
-    'license': 'MIT',
-    'keywords': 'test automation selenium tir totvs protheus framework',
-    'classifiers':[
+    version='1.13.0.post7',
+    license='MIT',
+    keywords='test automation selenium tir totvs protheus framework',
+    classifiers=[
         'Environment :: Web Environment',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -28,21 +28,19 @@ config = {
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing'
     ],
-    'install_requires': [
-        'beautifulsoup4==4.7.1',
-        'bs4==0.0.1',
+    install_requires=[
+        'beautifulsoup4>=4.7.1',
+        'bs4>=0.0.1',
         'numpy',
-        'pandas==0.23.4',
-        'python-dateutil==2.6.1',
-        'pytz==2017.3',
-        'selenium==3.8.0',
+        'pandas>=0.23.4',
+        'python-dateutil>=2.6.1',
+        'pytz>=2017.3',
+        'selenium>=3.8.0',
         'six==1.11.0',
         'enum34',
         'requests'
     ],
-    'packages': ['tir'],
-    'scripts': [],
-    'name': 'tir'
-}
-
-setup(**config, include_package_data=True)
+    packages=['tir'],
+    scripts=[],
+    name='tir_framework'
+)
