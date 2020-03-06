d4b74058f04285311d92b307d7fbd333723f24d0
Renan Lacerda Lisboa <renanllisboa@gmail.com>
2020-03-06 15:08:42 -0300

    upgrade release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     project_urls={
     'Script Samples': 'https://github.com/totvs/tir-script-samples'
     },
-    version='1.13.0-3',
+    version='1.14.0',
     license='MIT',
     keywords='test automation selenium tir totvs protheus framework',
     classifiers=[
