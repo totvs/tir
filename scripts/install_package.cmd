05189ae935b9eff4680845a1ed8bb8094f902bcb
Renan Lacerda Lisboa <renanllisboa@gmail.com>
2020-04-29 15:45:53 -0300

    added a generic exception if enter key fail.

--- scripts/install_package.cmd
+++ scripts/install_package.cmd
@@ -13,5 +13,5 @@ taskkill /f /im chromedriver.exe
 echo -------------------------
 echo Installing project...
 echo -------------------------
-pip install -U dist/tir_framework-1.15.10.tar.gz
+pip install -U dist/tir_framework-1.15.11.tar.gz
 pause >nul | set/p = Press any key to exit ...
