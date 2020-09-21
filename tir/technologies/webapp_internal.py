79c25cbae186eff778f56467153946f2bd4c1d38
Renan Lacerda Lisboa <renanllisboa@gmail.com>
2020-09-21 14:37:55 -0300

    add timeout and scroll to element in checkresult.

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -1740,8 +1740,9 @@ class WebappInternal(Base):
                 self.log_error(f"Couldn't find element: {field}")
 
             field_element = lambda: self.driver.find_element_by_xpath(xpath_soup(element))
-
-            endtime = time.time() + 10
+            self.set_element_focus(label_element())
+            self.scroll_to_element(field_element())
+            endtime = time.time() + self.config.time_out
             current_value =  ''
             while(time.time() < endtime and not current_value):
                 current_value = self.get_web_value(field_element()).strip()
