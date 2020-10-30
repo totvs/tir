64d06755bb59d14a5d81f3a421a08071a34f951e
Wilson08 <wilson.ortega08@gmail.com>
2020-10-30 10:30:35 -0300

    change version

--- doc_files/source/conf.py
+++ doc_files/source/conf.py
@@ -30,7 +30,7 @@ author = 'Totvs Automation Team'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.17.3'
+release = '1.17.23'
 
 
 # -- General configuration ---------------------------------------------------
