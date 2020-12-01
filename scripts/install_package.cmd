0e72da6d2e60ed0bf2828799c44b7a751097ae32
HelenaAdrignoli <hadrignoli@gmail.com>
2020-12-01 14:44:56 -0300

    Release Upgrade

--- scripts/install_package.cmd
+++ scripts/install_package.cmd
@@ -13,5 +13,5 @@ taskkill /f /im chromedriver.exe
 echo -------------------------
 echo Installing project...
 echo -------------------------
-pip install -U dist/tir_framework-1.17.31.tar.gz
+pip install -U dist/tir_framework-1.17.32.tar.gz
 pause >nul | set/p = Press any key to exit ...
