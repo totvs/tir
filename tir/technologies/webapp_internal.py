d606fcc8a284dabdd6fec2972ffc360debc712b4
98llm <limamonteiro2010@hotmail.com>
2022-05-20 14:33:27 -0300

    fix database environment

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -835,8 +835,7 @@ class WebappInternal(Base):
                 True, twebview=True)
         else:
             optional_term = "wa-button" if self.webapp_shadowroot() else "button"
-            buttons = self.web_scrap(label, scrap_type=enum.ScrapType.MIXED, optional_term=optional_term,
-                                     main_container="body")
+            buttons = self.web_scrap(label, scrap_type=enum.ScrapType.MIXED, optional_term=optional_term, second_term='button', main_container="body")
             buttons = list(filter(lambda x: self.element_is_displayed(x), buttons ))
 
         button_element = next(iter(buttons), None) if buttons else None
@@ -856,12 +855,17 @@ class WebappInternal(Base):
         if self.config.poui_login:
             self.switch_to_iframe()
         
+        click = 1
         tryng = 1
+        endtime = time.time() + self.config.time_out
         while time.time() < endtime and self.element_is_displayed(button()):
-            self.click(button(), enum.ClickType.ACTIONCHAINS)
-            logger().info(f'click on {button().text} ,tentativa {tryng}')
-            tryng+=1
+            self.click(button(), enum.ClickType(click))
+            logger().info(f'click on {button().text}, tentativa{tryng}')
+            tryng += 1
+            click += 1
             time.sleep(2)
+            if click == 4:
+                click = 1
 
         if not self.config.poui_login:
             if self.webapp_shadowroot():
