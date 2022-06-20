290c4541d521bc2225bb7677bb09d412ede26350
98llm <limamonteiro2010@hotmail.com>
2022-06-20 09:49:38 -0300

    clickcheckBox fix

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -7885,12 +7885,8 @@ class WebappInternal(Base):
             regex = r"(<[^>]*>)?([\?\*\.\:]+)?"
             label_text =  re.sub(regex, '', label_text)
 
-            wa_text_view = container.select('wa-text-view')
+            wa_text_view = container.select('wa-text-view, wa-checkbox')
             wa_text_view_filtered = list(filter(lambda x: hasattr(x, 'caption') and re.sub(regex, '', x['caption']).lower().strip().startswith(label_text.lower().strip()), wa_text_view))
-            
-            if not wa_text_view_filtered:
-                wa_text_view = container.select('wa-panel>wa-checkbox')
-                wa_text_view_filtered = list(filter(lambda x:  hasattr(x, 'caption') and re.search(regex , x['caption']), wa_text_view))
 
             if len(wa_text_view_filtered) > 1:
                 wa_text_view_filtered = list(filter(lambda x:  hasattr(x, 'caption') and re.sub(regex, '', x['caption']).lower().strip() == (label_text.lower().strip()), wa_text_view))
