985bf097c583cac956272d98a19d548bcf39b89f
renanllisboa <renanllisboa@gmail.com>
2023-02-08 14:26:56 -0300

    add merge_date_mask method. add date format method.

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -313,15 +313,30 @@ class WebappInternal(Base):
         :param date:
         :return:
         """
-        pattern_ddmmaaaa = '([\d]{2}).?([\d]{2}).?([\d]{4})'
-        pattern_ddmmaa = '([\d]{2}).?([\d]{2}).?([\d]{2})'
+        pattern_1 = '([\d]{2}).?([\d]{2}).?([\d]{4})'
+        pattern_2 = '([\d]{2}).?([\d]{2}).?([\d]{2})'
 
-        date_formated = re.sub(pattern_ddmmaaaa, r'\1/\2/\3', date)
+        date_formated = re.sub(pattern_1, r'\1/\2/\3', date)
 
-        if not re.match(pattern_ddmmaaaa, date_formated):
-            date_formated = re.sub(pattern_ddmmaa, r'\1/\2/\3', date)
+        if not re.match(pattern_1, date_formated):
+            date_formated = re.sub(pattern_2, r'\1/\2/\3', date)
 
         return date_formated
+
+    def merge_date_mask(self, base_date, date):
+
+
+        pattern_1 = r"\d{2}/\d{2}/\d{4}"
+        pattern_2 = r"\d{2}/\d{2}/\d{2}"
+
+        match1 = re.match(pattern_1, base_date)
+        match2 = re.match(pattern_2, base_date)
+
+        if match1:
+            return date
+        elif match2:
+            split_date = date.split('/')
+            return f"{split_date[0]}/{split_date[1]}/{split_date[-1][-2:]}"
 
     def close_screen_before_menu(self):
 
@@ -817,7 +832,7 @@ class WebappInternal(Base):
                 ActionChains(self.driver).key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys(
                     Keys.END).key_up(Keys.CONTROL).key_up(Keys.SHIFT).perform()
                 self.send_keys(date(), self.config.date)
-                base_date_value = self.get_web_value(date())
+                base_date_value = self.merge_date_mask(self.config.date, self.get_web_value(date()))
                 if self.config.poui_login:
                     ActionChains(self.driver).send_keys(Keys.TAB).perform()
 
