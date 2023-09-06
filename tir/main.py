6c1b3e618e257f5562a1e71bdb3c2fa724382db0
renanllisboa <renanllisboa@gmail.com>
2023-09-06 17:05:33 -0300

    fix merge

--- tir/main.py
+++ tir/main.py
@@ -1336,7 +1336,7 @@ class Webapp():
         >>> my_grid = self.get_grid()
         >>> my_grid = self.get_grid(grid_element='.dict-msbrgetdbase')
         """
-        
+
         return self.__webapp.get_grid_content(grid_number, grid_element)
 
     def LengthGridLines(self, grid):
@@ -1346,6 +1346,64 @@ class Webapp():
         """
 
         return self.__webapp.LengthGridLines(grid)
+
+    def InputByLocator(self, selector='', locator='', value=''):
+        """
+
+        .. note::
+            Necessary import "By" class in the script: from tir.technologies.core.base import By
+
+        .. note::
+            For more information check this out: https://selenium-python.readthedocs.io/locating-elements.html
+
+        .. warning::
+            Use only in cases where it is not possible to use a label or name attribute.
+            Any interface change can directly impact the script. Evaluate the possibility of changing the interface
+            before using these methods in the script.
+
+        :param selector: The type of selector to use (e.g., 'css', 'xpath', 'id').
+        :type selector: str
+        :param locator: The locator expression to identify the element. (e.g., By.CSS_SELECTOR, By.ID)
+        :type locator: str
+        :param value: The value to be used (e.g., for input or interaction).
+        :type value: str
+
+        Usage:
+
+        >>> # Calling the method:
+        >>> oHelper.InputByLocator(selector='COMP7526', locator=By.ID, value='teste')
+        :return: None
+        """
+        return self.__webapp.filling_input_by_locator(selector, locator, value)
+
+    def ClickByLocator(self, selector='', locator='', right_click=False):
+        """
+
+        .. note::
+            Necessary import "By" class in the script: from tir.technologies.core.base import By
+
+        .. note::
+            For more information check this out: https://selenium-python.readthedocs.io/locating-elements.html
+
+        .. warning::
+            Use only in cases where it is not possible to use a label or name attribute.
+            Any interface change can directly impact the script. Evaluate the possibility of changing the interface
+            before using these methods in the script.
+
+        :param selector: The type of selector to use (e.g., 'css', 'xpath', 'id').
+        :type selector: str
+        :param locator: The locator expression to identify the element. (e.g., By.CSS_SELECTOR, By.ID)
+        :type locator: str
+        :param right_click: Perform a right-click action if True (default is False).
+        :type right_click: bool
+
+        Usage:
+
+        >>> # Calling the method:
+        >>> oHelper.ClickByLocator(selector='COMP7536', locator=By.ID)
+        :return: None
+        """
+        return self.__webapp.click_by_locator(selector, locator, right_click)
 
 class Apw():
 
