18afe1b5686146d37ce3d668fc4a85cbcd964bea
Helena Adrignoli Leal <helena.leal@totvs.com.br>
2023-09-25 14:29:12 -0300

    hotfix/close_coin

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -1200,10 +1200,10 @@ class WebappInternal(Base):
         modals = self.zindex_sort(soup.select(selector), True)
         if modals and self.element_exists(term=self.language.coins, scrap_type=enum.ScrapType.MIXED,
         optional_term=selector, main_container="body", check_error = False):
-            self.SetButton(self.language.confirm)
-        else:
             if self.config_language == 'ru-ru':
                 self.SetButton(self.language.shortconfirm)
+            else:
+                self.SetButton(self.language.confirm)
 
 
     def close_coin_screen_after_routine(self):
