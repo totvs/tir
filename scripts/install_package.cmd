03d395af05929cb83b6af8d54029d1b54b9a7073
renanllisboa <renanllisboa@gmail.com>
2023-11-03 15:28:08 -0300

    branch release v1.20.16rc1

--- scripts/install_package.cmd
+++ scripts/install_package.cmd
@@ -13,5 +13,5 @@ taskkill /f /im chromedriver.exe
 echo -------------------------
 echo Installing project...
 echo -------------------------
-pip install -U dist/tir_framework-1.20.15.tar.gz
+pip install -U dist/tir_framework-1.20.16rc1.tar.gz
 pause >nul | set/p = Press any key to exit ...
