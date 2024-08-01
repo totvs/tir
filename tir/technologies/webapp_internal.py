a46edd1c1e9d1f6e3924e8ab17f06aec8f79912a
98llm <limamonteiro2010@hotmail.com>
2024-08-01 11:09:17 -0300

    back reversed

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -7530,6 +7530,7 @@ class WebappInternal(Base):
                 filtered_rows = list(filter(lambda x: "selected-row" == self.soup_to_selenium(x).get_attribute('class'), rows))
                 if filtered_rows:
                     return next(iter(list(filter(lambda x: "selected-row" == self.soup_to_selenium(x).get_attribute('class'), rows))), None)
+        return next(reversed(rows), None)
 
 
     def SetFilePath(self, value, button = ""):
