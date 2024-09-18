7132546468daa56a9aaa6c1058b1b24e639e836e
renanllisboa <renanllisboa@gmail.com>
2024-09-18 18:17:09 -0300

    merge feature/remove_scroll_to_element

---
+++ scripts/install_package.cmd
@@ -0,0 +1,17 @@
+@echo off
+title TIR installer
+echo -------------------------
+cd ..
+echo Building project...
+echo -------------------------
+python setup.py sdist
+echo -------------------------
+echo Terminating possible open driver instances
+echo -------------------------
+taskkill /f /im geckodriver.exe
+taskkill /f /im chromedriver.exe
+echo -------------------------
+echo Installing project...
+echo -------------------------
+pip install -U dist/tir_framework-1.20.25rc1.tar.gz
+pause >nul | set/p = Press any key to exit ...
