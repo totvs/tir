487f3e890e27d352cce3d260219b73228200499a
98llm <limamonteiro2010@hotmail.com>
2025-01-21 16:58:41 -0300

    take

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -870,9 +870,9 @@ class WebappInternal(Base):
                         ActionChains(self.driver).key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys(
                             Keys.END).key_up(Keys.CONTROL).key_up(Keys.SHIFT).perform()
 
-                        self.take_screenshot(f'fill_data_before_send{click_type}_{time.time()}')
+                        self.take_screenshot(f'fill_data_before_send{send_type}_{time.time()}')
                         self.try_send_keys(date, self.config.date, try_counter=send_type)
-                        self.take_screenshot(f'fill_data_after_send{click_type}_{time.time()}')
+                        self.take_screenshot(f'fill_data_after_send{send_type}_{time.time()}')
 
                         base_date_value = self.merge_date_mask(self.config.date, self.get_web_value(date()))
                         if self.config.poui_login:
