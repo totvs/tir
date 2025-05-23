540d63cd114e57e2996aecb21d417ceb98cff11a
98llm <limamonteiro2010@hotmail.com>
2025-05-23 15:55:39 -0300

    fix ident

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -2957,8 +2957,8 @@ class WebappInternal(Base):
                     interface_value = self.get_web_value(input_field())
 
                 current_value = interface_value.strip()
-                    get_max_lenght = lambda: self.driver.execute_script('return arguments[0]._maxLength', input_field())
-                    interface_value_size = get_max_lenght() if input_field().tag_name != 'textarea' else len(value)+1
+                get_max_lenght = lambda: self.driver.execute_script('return arguments[0]._maxLength', input_field())
+                interface_value_size = get_max_lenght() if input_field().tag_name != 'textarea' else len(value) + 1
                 user_value_size = len(value)
 
                 if valtype == 'D' and user_value_size > interface_value_size:
