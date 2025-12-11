1dbdb870d043fc02843699045741aefdd13f5afb
Vinicius Oliveira <v.deoliveira@totvs.com.br>
2025-12-11 16:12:37 -0300

    Remove the condition that only closes warnings if the initial program is SIGAADV of Program function.

--- tir/technologies/core/router.py
+++ tir/technologies/core/router.py
@@ -113,7 +113,7 @@ class Router:
         drv = self._route_to_driver(lambda: self.config.new_home)
         drv.Program(program_name)
 
-        if self.config.new_home and self.config.initial_program.lower() == 'sigaadv':
+        if self.config.new_home:
             self._ensure_webapp().close_warning_screen_after_routine()
             self._ensure_webapp().close_coin_screen_after_routine()
             self._ensure_webapp().close_news_screen_after_routine()
