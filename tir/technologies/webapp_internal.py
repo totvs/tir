a2838cfc7bd3ddb47c7539f5c8d2977c0c39ab1b
Vinicius Oliveira <v.deoliveira@totvs.com.br>
2026-08-20 15:26:12 -0300

    fix(webapp_internal): add error handling for Enter key in select field

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -3402,9 +3402,13 @@ class WebappInternal(Base):
                            self.select_combo(element, main_value, index=True)
                         else:
                             self.select_combo(element, main_value)
-                        
-                        self.set_element_focus(input_field())
-                        ActionChains(self.driver).send_keys(Keys.ENTER).perform()
+
+                        try:
+                            self.set_element_focus(input_field())
+                            ActionChains(self.driver).send_keys(Keys.ENTER).perform()
+
+                        except Exception as e:
+                            logger().debug(f"An error occurred when pressing the Enter key in the select field: {e}")
 
                         current_value = self.return_selected_combo_value(element).strip()
                     #Action for Input elements
