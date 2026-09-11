1618b179a1817621e4fc1da573693a7e71791f97
Vinicius Oliveira <v.deoliveira@totvs.com.br>
2026-09-11 14:35:45 -0300

    fix(webapp_internal): add periodic screen reload during user element lookup

--- tir/technologies/webapp_internal.py
+++ tir/technologies/webapp_internal.py
@@ -608,8 +608,22 @@ class WebappInternal(Base):
         try_counter = 0
         user_value = ''
         user_element = None
+        reload_screen_interval = 120  # 2 minutes
         endtime = time.time() + self.config.time_out
+        next_reload_time = time.time() + reload_screen_interval
+
         while (time.time() < endtime and (user_value.strip() != user_text.strip())):
+
+            if time.time() >= next_reload_time:
+                logger().debug("User element not found, reloading user screen...")
+
+                self.reload_user_screen()
+
+                next_reload_time = time.time() + reload_screen_interval
+                user_element = None
+                user_value = ''
+                try_counter = 0
+                continue
 
             logger().debug("Looking for user element...")
 
@@ -625,7 +639,7 @@ class WebappInternal(Base):
                     user_element = next(iter(soup.select(get_user)), None)
 
                 if user_element is None:
-                    time.sleep(0.5)
+                    time.sleep(1)
                     continue
 
             except AttributeError as e:
